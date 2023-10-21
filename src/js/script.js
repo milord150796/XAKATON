028c46005c5662f4eeb4a829da2d5d1e8fcdff10
Ihor <oleksandrenko1996@gmail.com>
2023-10-21 15:06:07 +0300

    add js

--- src/js/script.js
+++ src/js/script.js
@@ -8,9 +8,14 @@ myXhr.onload = function () {
     const myObject = {
       pageContent: myXhr.response
     };
-    console.log('Отримані дані:', JSON.parse(myObject.pageContent));
+    let tranport_typ = JSON.parse(myObject.pageContent);
+    let transpot_typ_document = document.getElementById("transpot-typ");
+    let html_text = tranport_typ.map((elem) => {
+      return `<option value="${elem.value}">${elem.name}</option>`;
+    });
+    transpot_typ_document.innerHTML = html_text.join(''); 
   } else {
-    console.error(Помилка: статус код - ${ myXhr.status });
+    console.error(`Помилка: статус код - ${myXhr.status}`);
   }
 };
 
@@ -19,3 +24,37 @@ myXhr.onerror = function () {
 };
 
 myXhr.send();
+
+let e = document.getElementById("transpot-typ");
+e.addEventListener("change", function() {
+  console.log("e.value", e.value);
+  const requestMarks = `https://developers.ria.com/auto/categories/${e.value}/marks?api_key=hPakOudm8AaUyt5aMie8MKqY0xtcIuDWowepg3pa`;
+const myXhrMarks = new XMLHttpRequest();
+myXhrMarks.open('GET', requestMarks, true);
+
+myXhrMarks.onload = function () {
+  if (myXhrMarks.status === 200) {
+    const Marks = {
+      pageContent: myXhrMarks.response
+    };
+    let tranport_marks = JSON.parse(Marks.pageContent);
+    let transpot_marks_document = document.getElementById("transpot-marks");
+    let html_text = tranport_marks.map((elem) => {
+      return `<option value="${elem.value}">${elem.name}</option>`;
+    });
+    transpot_marks_document.innerHTML = html_text.join(''); 
+  } else {
+    console.error(`Помилка: статус код - ${myXhrMarks.status}`);
+  }
+};
+
+myXhrMarks.onerror = function () {
+  console.error('Помилка запиту');
+};
+
+myXhrMarks.send();
+let tranport_marks_click = document.getElementById("transpot-marks");
+tranport_marks_click.addEventListener("change",function(){
+  console.log("value_2",tranport_marks_click.value)
+})
+});
